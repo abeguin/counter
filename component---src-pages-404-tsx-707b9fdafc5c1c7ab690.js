62193985e6595182fbae281e8aca79f88dd6b873
Arnaud Béguin <abeguin@kleis.ch>
2021-12-08 23:55:28 +0100

    Updates

---
+++ component---src-pages-404-tsx-707b9fdafc5c1c7ab690.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcounter=self.webpackChunkcounter||[]).push([[218],{9786:function(e,t,n){n.r(t);var u=n(7294),l=n(6722),r=n(471);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-707b9fdafc5c1c7ab690.js.map
